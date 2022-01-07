11ca59886ebac019bbb021343bda21f1a8755b3c
nyazadzhiev <nyazadzhiev@gmail.com>
2022-01-07 11:35:58 +0200

    Created enums

---
+++ Assignment 9/Workforce-management/src/WorkforceManagementAPI.DAL/Entities/Enums/RequestType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkforceManagementAPI.DAL.Entities.Enums
+{
+    public enum RequestType
+    {
+        Paid = 1,
+        NonPaid = 0,
+        SickLeave = 2
+    }
+}
