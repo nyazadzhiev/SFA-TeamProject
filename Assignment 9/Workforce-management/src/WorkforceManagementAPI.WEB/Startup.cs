d906564bb7edbeb678622f01b7661d583862dd03
nyazadzhiev <nyazadzhiev@gmail.com>
2022-01-07 13:05:53 +0200

    Added dbContext to startup

--- Assignment 9/Workforce-management/src/WorkforceManagementAPI.WEB/Startup.cs
+++ Assignment 9/Workforce-management/src/WorkforceManagementAPI.WEB/Startup.cs
@@ -1,9 +1,14 @@
+using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using WorkforceManagementAPI.DAL;
+using WorkforceManagementAPI.DAL.Entities;
+using Microsoft.AspNetCore.Identity;
 
 namespace WorkforceManagementAPI.WEB
 {
@@ -25,6 +30,13 @@ namespace WorkforceManagementAPI.WEB
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WorkforceManagementAPI.WEB", Version = "v1" });
             });
+
+            services.AddDbContext<DatabaseContext>();
+
+            var database = new DatabaseContext(Configuration);
+            DatabaseSeeder.Seed(database);
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
