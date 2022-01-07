02a64c2a23c2544b5ea043d5e4b3f6ab2416040d
nyazadzhiev <nyazadzhiev@gmail.com>
2022-01-07 10:55:59 +0200

    Created Entities

---
+++ Assignment 9/Workforce-management/src/WorkforceManagementAPI.DAL/Entities/TimeOff.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkforceManagementAPI.DAL.Entities
+{
+    public class TimeOff
+    {
+        public int Id { get; set; }
+
+        public string Reason { get; set; }
+
+        public int Type { get; set; }
+
+        public int Status { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime ModifiedAt { get; set; }
+
+        public int CreatorId { get; set; }
+
+        public virtual User Creator { get; set; }
+
+        public int ModifierId { get; set; }
+
+        public virtual User Modifier { get; set; }
+
+        public List<User> Reviewers { get; set; }
+
+        public TimeOff()
+        {
+            this.Reviewers = new List<User>();
+        }
+    }
+}
