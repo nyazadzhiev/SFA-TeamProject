7e7b674309782baee87387fe8ce2b7ebcf972672
nyazadzhiev <nyazadzhiev@gmail.com>
2022-01-07 13:05:38 +0200

    Implemented DB setup and seeder

---
+++ Assignment 9/Workforce-management/src/WorkforceManagementAPI.DAL/DatabaseSeeder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkforceManagementAPI.DAL.Entities;
+
+namespace WorkforceManagementAPI.DAL
+{
+    public class DatabaseSeeder
+    {
+        public static void Seed(DatabaseContext database)
+        {
+            if (database.Database.EnsureCreated())
+            {
+                database.Users.Add(new User()
+                {
+                    Email = "admin@admin.test",
+                    Username = "admin@admin.test",
+                    Password = "adminpass",
+                    FirstName = "Admin",
+                    LastName = "Admin",
+                }); 
+
+                database.SaveChanges();
+            }
+        }
+
+    }
+}
