336bb7593278e367795078a42a3932c2029ffa41
nyazadzhiev <nyazadzhiev@gmail.com>
2022-01-07 19:18:02 +0200

    Renamed TimeOff to Request

---
+++ Assignment 9/Workforce-management/src/WorkforceManagementAPI.DAL/Entities/Request.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkforceManagementAPI.DAL.Entities
+{
+    public class Request
+    {
+        public Guid Id { get; set; }
+
+        public string Reason { get; set; }
+
+        public int Type { get; set; }
+
+        public int Status { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime ModifiedAt { get; set; }
+
+        public Guid CreatorId { get; set; }
+
+        public virtual User Creator { get; set; }
+
+        public Guid ModifierId { get; set; }
+
+        public virtual User Modifier { get; set; }
+
+        public virtual List<User> Reviewers { get; set; }
+
+        public Request()
+        {
+            this.Reviewers = new List<User>();
+        }
+    }
+}
