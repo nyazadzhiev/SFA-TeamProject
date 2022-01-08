3e9f7d7e299c24a41f89001e11119cd650860e87
Nikolay Shtipkov <nikolayshtipkov@gmail.com>
2022-01-08 12:05:20 +0200

    Add services folder and TeamService class sceleton

---
+++ Assignment 9/Workforce-management/src/WorkforceManagementAPI.BLL/Services/TeamService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkforceManagementAPI.DAL;
+
+namespace WorkforceManagementAPI.BLL.Service
+{
+    public class TeamService
+    {
+        private readonly DatabaseContext _context;
+
+        public TeamService(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> CreateTeamAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> EditTeamAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> DeleteTeamAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
