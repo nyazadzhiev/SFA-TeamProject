4ef9098b6b145125a441469520297599a254162d
nyazadzhiev <nyazadzhiev@gmail.com>
2022-01-09 11:47:53 +0200

    Created response messages

---
+++ Assignment 9/Workforce-management/src/WorkforceManagementAPI.Common/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkforceManagementAPI.Common
+{
+    public static class Constants
+    {
+        public const string NotFound = "{0} not found";
+
+        public const string NameAlreadyInUse = "{0} is already in use.";
+    }
+}
