16645f8a485391376c46eb369f064f658a195683
nyazadzhiev <nyazadzhiev@gmail.com>
2022-01-09 15:29:45 +0200

    Created IValidationService Interface

---
+++ Assignment 9/Workforce-management/src/WorkforceManagementAPI.BLL/Contracts/IValidationService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkforceManagementAPI.DAL.Entities;
+
+namespace WorkforceManagementAPI.BLL.Contracts
+{
+    public interface IValidationService
+    {
+        void EnsureTeamExist(Team team);
+
+        void CheckTeamName(string title);
+    }
+}
