1a9ac20f87b237c58a4ce1dfaeb4816115880469
nyazadzhiev <nyazadzhiev@gmail.com>
2022-01-09 22:30:43 +0200

    Created timeoff service interface

---
+++ Assignment 9/Workforce-management/src/WorkforceManagementAPI.BLL/Contracts/ITimeOffService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkforceManagementAPI.BLL.Contracts
+{
+    public interface ITimeOffService
+    {
+    }
+}
