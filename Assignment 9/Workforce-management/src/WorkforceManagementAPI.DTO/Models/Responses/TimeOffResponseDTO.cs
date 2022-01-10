55479cb54418d48ff4726b4384b9bc2000e8a4e2
nyazadzhiev <nyazadzhiev@gmail.com>
2022-01-10 21:05:25 +0200

    Created timeoff DTOs

---
+++ Assignment 9/Workforce-management/src/WorkforceManagementAPI.DTO/Models/Responses/TimeOffResponseDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using WorkforceManagementAPI.DAL.Entities.Enums;
+
+namespace WorkforceManagementAPI.DTO.Models.Responses
+{
+    public class TimeOffResponseDTO
+    {
+        public string Reason { get; set; }
+
+        public RequestType Type { get; set; }
+
+        public Status Status { get; set; }
+
+        public string CreatorName { get; set; }
+
+        public string ModifierName { get; set; }
+
+        public DateTime startDate { get; set; }
+
+        public DateTime endDate { get; set; }
+    }
+}
