bcf68c32f48cda8698537a1d3f55153a9f559e51
Atanas Mushamov <nasko_mp4@abv.bg>
2022-01-10 16:34:49 +0200

    Inicial setup for user controller

---
+++ Assignment 9/Workforce-management/src/WorkforceManagementAPI.WEB/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WorkforceManagementAPI.BLL.Services;
+
+namespace WorkforceManagementAPI.WEB.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize(Roles = "Admin")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UsersController(IUserService userService) : base()
+        {
+            _userService = userService;
+        }
+
+
+    }
+}
