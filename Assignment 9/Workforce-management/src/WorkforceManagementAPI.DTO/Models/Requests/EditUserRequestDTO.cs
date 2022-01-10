9f46d989aca2fde80529255246b27bd685cee27b
Atanas Mushamov <nasko_mp4@abv.bg>
2022-01-10 17:00:13 +0200

    Added edit user dto

---
+++ Assignment 9/Workforce-management/src/WorkforceManagementAPI.DTO/Models/Requests/EditUserRequestDTO.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WorkforceManagementAPI.DTO.Models.Requests
+{
+    public class EditUserReauestDTO : IValidatableObject
+    {
+        [Required]
+        [MinLength(5)]
+        [MaxLength(50)]
+        public string NewEmail { get; set; }
+
+        [Required2]
+        [MinLength(8)]
+        [MaxLength(50)]
+        public string NewPassword { get; set; }
+
+        [Required2]
+        [MinLength(8)]
+        [MaxLength(50)]
+        public string RepeatPassword { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(50)]
+        public string NewFirstName { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(50)]
+        public string NewLastName { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            List<ValidationResult> result = new List<ValidationResult>();
+            if (NewPassword != RepeatPassword)
+            {
+                result.Add(new ValidationResult("Passwords do not match", new string[] { "Password" }));
+            }
+            return result;
+        }
+
+        public class Required2Attribute : RequiredAttribute
+        {
+            public override bool IsValid(object value)
+            {
+                return base.IsValid(value);
+            }
+            protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+            {
+                return base.IsValid(value, validationContext);
+            }
+        }
+
+
+    }
+}
