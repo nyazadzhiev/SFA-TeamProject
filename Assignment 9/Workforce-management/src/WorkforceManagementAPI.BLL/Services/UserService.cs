7a0fbc99a56e1e3409221454d2ee8403c9ba77d9
Atanas Mushamov <nasko_mp4@abv.bg>
2022-01-10 09:02:55 +0200

    Inicial setup for user service

---
+++ Assignment 9/Workforce-management/src/WorkforceManagementAPI.BLL/Services/UserService.cs
@@ -0,0 +1,16 @@
+﻿using WorkforceManagementAPI.DAL.Contracts.IdentityContracts;
+
+namespace WorkforceManagementAPI.BLL.Services
+{
+    public class UserService
+    {
+        private readonly IIdentityUserManager _userManager;
+
+        public UserService(IIdentityUserManager userManager)
+        {
+            _userManager = userManager;
+        }
+
+
+    }
+}
