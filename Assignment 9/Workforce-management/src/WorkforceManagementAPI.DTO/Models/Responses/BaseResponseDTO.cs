d0215e0a060f1bb64b7bbcfe4653a8594bd6f7dd
Nikolay Shtipkov <nikolayshtipkov@gmail.com>
2022-01-10 14:50:36 +0200

    Add team controller, request/response DTO files

---
+++ Assignment 9/Workforce-management/src/WorkforceManagementAPI.DTO/Models/Responses/BaseResponseDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkforceManagementAPI.DTO.Models.Responses
+{
+    public class BaseResponseDTO
+    {
+        public int Id { get; set; }
+
+        public int CreatorId { get; set; }
+
+        public int ModifierId { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime ModifierAt { get; set; }
+    }
+}
---
+++ Assignment 9/Workforce-management/src/WorkforceManagementAPI.WEB/Controllers/TeamController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorkforceManagementAPI.BLL.Service;
+using WorkforceManagementAPI.BLL.Services.IdentityServices;
+
+namespace WorkforceManagementAPI.WEB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeamController : ControllerBase
+    {
+        private static IdentityUserManager _userService;
+        private static TeamService _teamService;
+
+        public TeamController(IdentityUserManager userService, TeamService teamService) : base()
+        {
+            _userService = userService;
+            _teamService = teamService;
+        }
+    }
+}
