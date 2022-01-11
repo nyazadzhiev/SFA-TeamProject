3555ee086bbccdec0669f5b687f366ee02d89129
nyazadzhiev <nyazadzhiev@gmail.com>
2022-01-11 16:19:01 +0200

    Implemented timeoff validation setup

--- Assignment 9/Workforce-management/src/WorkforceManagementAPI.Test/ServicesTestBase.cs
+++ Assignment 9/Workforce-management/src/WorkforceManagementAPI.Test/ServicesTestBase.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using WorkforceManagementAPI.BLL.Service;
 using WorkforceManagementAPI.BLL.Services;
 using WorkforceManagementAPI.DAL;
+using WorkforceManagementAPI.DAL.Contracts.IdentityContracts;
 using WorkforceManagementAPI.DAL.Entities;
 
 namespace WorkforceManagementAPI.Test
@@ -17,19 +18,26 @@ namespace WorkforceManagementAPI.Test
     {
         public Team regularTeam { get; set; }
 
+        public TimeOff testTimeOff { get; set; }
+
         public ServicesTestBase()
         {
             this.regularTeam = new Team()
             {
                 Title = "testteam"
             };
+
+            this.testTimeOff = new TimeOff()
+            {
+            };
         }
 
         protected ValidationService SetupMockedDefaultValidationService()
         {
-            var configMock = new Mock<IConfiguration>();
-            var dbContextMock = new Mock<DatabaseContext>(configMock.Object);
-            var validation = new ValidationService(dbContextMock.Object);
+            var dbContextMock = new Mock<DatabaseContext>();
+            var mockedManager = new Mock<IIdentityUserManager>();
+
+            var validation = new ValidationService(dbContextMock.Object, mockedManager.Object);
 
             return validation;
         }
@@ -39,10 +47,31 @@ namespace WorkforceManagementAPI.Test
             List<Team> teams = new List<Team>();
             teams.Add(regularTeam);
 
-            var configMock = new Mock<IConfiguration>();
-            var mockContext = new Mock<DatabaseContext>(configMock.Object);
+            var mockContext = new Mock<DatabaseContext>();
             mockContext.Setup(m => m.Teams).Returns(() => ToDbSet(teams));
 
+
+            return mockContext.Object;
+        }
+
+        protected DatabaseContext SetupDefaultMockedDBTimeOffServiceAsync()
+        {
+            List<TimeOff> requests = new List<TimeOff>();
+
+            var mockContext = new Mock<DatabaseContext>();
+            mockContext.Setup(m => m.Requests).Returns(() => ToDbSet(requests));
+
+
+            return mockContext.Object;
+        }
+
+        protected DatabaseContext SetupMockedDBTimeOffServiceAsync()
+        {
+            List<TimeOff> requests = new List<TimeOff>();
+            requests.Add(testTimeOff);
+
+            var mockContext = new Mock<DatabaseContext>();
+            mockContext.Setup(m => m.Requests).Returns(() => ToDbSet(requests));
 
             return mockContext.Object;
         }
@@ -57,6 +86,20 @@ namespace WorkforceManagementAPI.Test
             mockSet.As<IQueryable<Team>>().Setup(m => m.ElementType).Returns(queryable.ElementType);
             mockSet.As<IQueryable<Team>>().Setup(m => m.GetEnumerator()).Returns(queryable.GetEnumerator());
             mockSet.Setup(d => d.Add(It.IsAny<Team>())).Callback<Team>(sourceList.Add);
+
+            return mockSet.Object;
+        }
+
+        private DbSet<TimeOff> ToDbSet(List<TimeOff> sourceList)
+        {
+            var queryable = sourceList.AsQueryable();
+
+            var mockSet = new Mock<DbSet<TimeOff>>();
+            mockSet.As<IQueryable<TimeOff>>().Setup(m => m.Provider).Returns(queryable.Provider);
+            mockSet.As<IQueryable<TimeOff>>().Setup(m => m.Expression).Returns(queryable.Expression);
+            mockSet.As<IQueryable<TimeOff>>().Setup(m => m.ElementType).Returns(queryable.ElementType);
+            mockSet.As<IQueryable<TimeOff>>().Setup(m => m.GetEnumerator()).Returns(queryable.GetEnumerator());
+            mockSet.Setup(d => d.Add(It.IsAny<TimeOff>())).Callback<TimeOff>(sourceList.Add);
 
             return mockSet.Object;
         }
