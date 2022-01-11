4cf068c41235b62c89c44d5fdf879c8c22d70ac8
nyazadzhiev <nyazadzhiev@gmail.com>
2022-01-11 16:17:31 +0200

    Added empty constructor to run tests

--- Assignment 9/Workforce-management/src/WorkforceManagementAPI.DAL/DatabaseContext.cs
+++ Assignment 9/Workforce-management/src/WorkforceManagementAPI.DAL/DatabaseContext.cs
@@ -9,8 +9,13 @@ namespace WorkforceManagementAPI.DAL
 
         public virtual DbSet<Team> Teams { get; set; }
 
-        public DbSet<TimeOff> Requests { get; set; }    
+        public virtual DbSet<TimeOff> Requests { get; set; }    
         public DatabaseContext(DbContextOptions<DatabaseContext> options) : base (options)
+        {
+
+        }
+
+        public DatabaseContext() : base()
         {
 
         }
