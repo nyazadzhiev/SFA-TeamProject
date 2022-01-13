7291072089fa82523ff94610cd61f8ec959e72fd
nyazadzhiev <nyazadzhiev@gmail.com>
2022-01-13 16:29:31 +0200

    Created new exception

---
+++ Assignment 9/Workforce-management/src/WorkforceManagementAPI.BLL/Exceptions/InvalidInputException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkforceManagementAPI.BLL.Exceptions
+{
+    public class InvalidInputException : Exception
+    {
+        public InvalidInputException(string message) : base(message)
+        {
+
+        }
+    }
+}
