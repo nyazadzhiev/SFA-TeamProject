ec571fb9e84e66dfcef82bea5cab5df664f7f739
Nikolay Shtipkov <nikolayshtipkov@gmail.com>
2022-01-14 17:49:06 +0200

    Add repository folder and TeamRepository file

---
+++ Assignment 9/Workforce-management/src/WorkforceManagementAPI.DAL/Repositories/TeamRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkforceManagementAPI.DAL.Repositories
+{
+    public class TeamRepository
+    {
+        private readonly DatabaseContext _context;
+
+        public TeamRepository(DatabaseContext context)
+        {
+            _context = context;
+        }
+    }
+}
