76cf3b5ebec620c669824c5211d354f45b1a589f
Atanas Mushamov <nasko_mp4@abv.bg>
2022-01-17 16:52:25 +0200

    Created time off profile

---
+++ Assignment 9/Workforce-management/src/WorkforceManagementAPI.WEB/AutoMapperProfiles/TimeOffProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using WorkforceManagementAPI.DAL.Entities;
+using WorkforceManagementAPI.DTO.Models.Requests;
+using WorkforceManagementAPI.DTO.Models.Responses;
+
+namespace WorkforceManagementAPI.WEB.AutoMapperProfiles
+{
+    public class TimeOffProfile : Profile
+    {
+        public TimeOffProfile()
+        {
+            CreateMap<TimeOff, TimeOffRequestDTO>()
+                .ReverseMap();
+
+            CreateMap<TimeOff, TimeOffResponseDTO>()
+                .ReverseMap();
+        }
+    }
+}
