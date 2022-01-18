528128911e429f6676643fd2a5f26fadbd928afe
Andreuwz <end17y1@gmail.com>
2022-01-18 23:11:23 +0200

    Removed deprecated unit tests. Created time off repository.

---
+++ Assignment 9/Workforce-management/src/WorkforceManagementAPI.DAL/Repositories/TimeOffRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WorkforceManagementAPI.DAL.Entities;
+
+namespace WorkforceManagementAPI.DAL.Repositories
+{
+    public  class TimeOffRepository
+    {
+        private readonly DatabaseContext _context;
+
+        public TimeOffRepository(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateTimeOffAsync(TimeOff timeOff)
+        {
+            await _context.Requests.AddAsync(timeOff);
+        }
+
+        public async Task<List<TimeOff>> GetAllAsync()
+        {
+            return await _context.Requests.ToListAsync();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<TimeOff>> GetMyTimeOffsAsync(string userId)
+        {
+            var timeOffs = await _context.Requests
+                .Where(ci => ci.CreatorId == userId)
+                    .ToListAsync();
+
+            return timeOffs;
+        }
+
+    }
+}
--- Assignment 9/Workforce-management/src/WorkforceManagementAPI.Test/TimeOffServiceTest.cs
+++ Assignment 9/Workforce-management/src/WorkforceManagementAPI.Test/TimeOffServiceTest.cs
@@ -1,15 +1,8 @@
 ﻿using AutoMapper;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Moq;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using WorkforceManagementAPI.BLL.Contracts;
 using WorkforceManagementAPI.BLL.Services;
-using WorkforceManagementAPI.DAL;
 using WorkforceManagementAPI.DAL.Entities;
 using WorkforceManagementAPI.DTO.Models.Requests;
 using Xunit;
@@ -18,79 +11,6 @@ namespace WorkforceManagementAPI.Test
 {
     public class TimeOffServiceTest : ServicesTestBase
     {
-        [Fact]
-        public async Task CreateTimeOff_Should_Return_True_When_TimeOff_Doesnt_Exist()
-        {
-            var mockDB = SetupDefaultMockedDBTimeOffServiceAsync();
-            var mockValidation = new Mock<IValidationService>();
-            var mockUserService = new Mock<IUserService>();
-            var mockNotification = new Mock<INotificationService>();
-            var mockMapper = new Mock<IMapper>();
-            mockMapper.Setup(m => m.Map<TimeOff>(It.IsAny<TimeOff>())).Returns(testTimeOff);
-
-            var model = new TimeOffRequestDTO()
-            {
-            };
-
-            var service = new TimeOffService(mockDB, mockValidation.Object, mockUserService.Object, mockNotification.Object, mockMapper.Object);
-
-            var result = await service.CreateTimeOffAsync(model, testTimeOff.CreatorId);
-            Assert.True(result);
-        }
-
-        [Fact]
-        public async Task DeleteTimeOff_Should_Return_True_When_TimeOff_Exist()
-        {
-            var mockDB = SetupMockedDBTimeOffServiceAsync();
-            var mockValidation = new Mock<IValidationService>();
-            var mockUserService = new Mock<IUserService>();
-            var mockNotification = new Mock<INotificationService>();
-            var mockMapper = new Mock<IMapper>();
-
-            var service = new TimeOffService(mockDB, mockValidation.Object, mockUserService.Object, mockNotification.Object, mockMapper.Object);
-
-            var result = await service.DeleteTimeOffAsync(testTimeOff.Id);
-
-            Assert.True(result);
-        }
-
-        [Fact]
-        public async Task EditTimeOff_Should_Return_True_When_TimeOff_Exist()
-        {
-            var mockDB = SetupMockedDBTimeOffServiceAsync();
-            var mockValidation = new Mock<IValidationService>();
-            var mockUserService = new Mock<IUserService>();
-            var mockNotification = new Mock<INotificationService>();
-            var mockMapper = new Mock<IMapper>();
-            mockMapper.Setup(m => m.Map<TimeOff>(It.IsAny<TimeOff>())).Returns(testTimeOff);
-
-            var model = new TimeOffRequestDTO()
-            {
-            };
-
-            var service = new TimeOffService(mockDB, mockValidation.Object, mockUserService.Object, mockNotification.Object, mockMapper.Object);
-
-
-            var result = await service.EditTimeOffAsync(testTimeOff.Id, model);
-
-            Assert.True(result);
-        }
-
-        [Fact]
-        public async Task GetTimeOff_Should_Return_Null_When_TimeOff_Doesnt_Exist()
-        {
-            var mockDB = SetupDefaultMockedDBTimeOffServiceAsync();
-            var mockValidation = new Mock<IValidationService>();
-            var mockUserService = new Mock<IUserService>();
-            var mockNotification = new Mock<INotificationService>();
-            var mockMapper = new Mock<IMapper>();
-            mockMapper.Setup(m => m.Map<TimeOff>(It.IsAny<TimeOff>())).Returns(testTimeOff);
-
-            var service = new TimeOffService(mockDB, mockValidation.Object, mockUserService.Object, mockNotification.Object, mockMapper.Object);
-
-            var result = await service.GetTimeOffAsync(testTimeOff.Id);
-
-            Assert.Null(result);
-        }
+        
     }
 }
