6eeab8104f12ada7fcb664af8ca27d869fe33da5
nyazadzhiev <nyazadzhiev@gmail.com>
2022-01-19 20:36:18 +0200

    Added new exception

---
+++ Assignment 9/Workforce-management/src/WorkforceManagementAPI.BLL/Exceptions/RequestCompletedException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkforceManagementAPI.BLL.Exceptions
+{
+    public class RequestCompletedException : Exception
+    {
+        public RequestCompletedException(string message) : base(message)
+        {
+
+        }
+    }
+}
